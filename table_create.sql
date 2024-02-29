4747498d12c8162d76097f7793fd12939f519232
Inés <146125296+inesguillen@users.noreply.github.com>
2024-02-29 20:41:20 +0100

    Update table_create.sql

--- table_create.sql
+++ table_create.sql
@@ -154,7 +154,7 @@ The deliveries table is a combination of the delivery_date and the delivery_addr
 
 CREATE TABLE Deliveries(
     delivery_date DATE NOT NULL,
-    delivery_address CHAR(120) NOT NULL,
+    delivery_address VARCHAR2(120) NOT NULL,
     CONSTRAINT PK_Deliveries PRIMARY KEY (delivery_date, delivery_address)
 );
 
@@ -219,7 +219,7 @@ Create Table Purchases(
     customer VARCHAR(255) NOT NULL,
 	mobile VARCHAR(255) NOT NULL,
     delivery_date DATE NOT NULL,
-    purchases_address CHAR(120) NOT NULL,
+    purchases_address VARCHAR2(120) NOT NULL,
     product_reference CHAR(15) NOT NULL,
     amount NUMBER NOT NULL,
     payment_date DATE,
